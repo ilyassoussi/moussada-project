103a9678af8e5e9c398d7c8269d9bad37b5db3bd
ilyassoussi <ilyassoussamaanass123@gmail.com>
2025-06-09 16:51:19 +0100

    ajouter le nom technicien

--- subventions-service/src/main/java/org/gov/moussaada/subventions_service/dao/DemandeTechniqueDAO.java
+++ subventions-service/src/main/java/org/gov/moussaada/subventions_service/dao/DemandeTechniqueDAO.java
@@ -5,8 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Map;
 
 public interface DemandeTechniqueDAO extends JpaRepository<Demande_technique,Integer> {
     @Query("FROM Demande_technique d WHERE d.isFinished = false")
     List<Demande_technique> NotFinished();
+
+    @Query("FROM Demande_technique d WHERE d.id_traitent_demande = :id")
+    Demande_technique findByIdTraitementDemande(int id);
+
+    @Query("FROM Demande_technique d WHERE d.id_reponse_technique = :id")
+    Demande_technique findByIdReponse(int id);
 }
--- subventions-service/src/main/java/org/gov/moussaada/subventions_service/service/inter/IDemandeTechnique.java
+++ subventions-service/src/main/java/org/gov/moussaada/subventions_service/service/inter/IDemandeTechnique.java
@@ -17,4 +17,6 @@ public interface IDemandeTechnique {
     ResponseEntity<?> getAllRapport();
 
     void validateRapport(int idRapport);
+
+    ResponseEntity<?> getByIdResponse(int id);
 }
