61eccce44fac8783bc5a23a7befa321ad29a0972
ilyassoussi <ilyassoussamaanass123@gmail.com>
2025-06-28 03:01:39 +0100

    declencher pipeline paysan

--- paysan-service/src/main/java/org/gov/moussaada/paysan_service/controller/InfoPaysan.java
+++ paysan-service/src/main/java/org/gov/moussaada/paysan_service/controller/InfoPaysan.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class InfoPaysan {
     private final TerrainSOAPService terrainSoapService;
 
+
     public InfoPaysan(TerrainSOAPService terrainSoapService) {
         this.terrainSoapService = terrainSoapService;
     }
